bf304632d351d8d17f4cf354398feb90f9084b6b
Shane Quick <shanesfast@gmail.com>
2019-11-03 22:20:01 +0000

    Add tests for validation of user sign in to access new form page / submit the form

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe GramsController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 
   describe "grams#create action" do
@@ -53,6 +58,11 @@ RSpec.describe GramsController, type: :controller do
       post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(gram_count).to eq 0
+    end
+
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end
